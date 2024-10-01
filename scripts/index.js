d63e3dc71abb19e8a2cba7ae1920f17d80a2a8c6
Wasted22 <art.dabizha@gmail.com>
2024-10-01 19:46:13 +0300

    fixed

--- scripts/index.js
+++ scripts/index.js
@@ -2,21 +2,22 @@
 const cardTemplate = document.querySelector('#card-template').content;
 const cardBlock = document.querySelector('.places__list');
 
-function addCard (data, DeleteCard){
+function addCard (data, deleteCard){
     const card = cardTemplate.querySelector('.card').cloneNode(true);
     const cardImage = card.querySelector('.card__image');
     const cardTitle = card.querySelector('.card__title');
     const deleteButton = card.querySelector('.card__delete-button');
-    deleteButton.addEventListener('click', DeleteCard)
+    deleteButton.addEventListener('click', deleteCard)
     cardImage.src = data.link;
+    cardImage.alt = data.name;
     cardTitle.textContent = data.name;
     return card;
 };
 
-function DeleteCard(evt){
+function deleteCard(evt){
     evt.target.parentElement.remove();
 }
 
 initialCards.forEach((data)=>{
-    cardBlock.append(addCard(data, DeleteCard));
+    cardBlock.append(addCard(data, deleteCard));
 });
